f215bc3fea364ada32639fc9ef05529585210be2
ziming <3409084730@qq.com>
2022-09-01 23:51:44 +0800

    封装了表单组件并在用户管理页面进行应用

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import {
   Button, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Dropdown,
-  DropdownMenu, DropdownItem, Row, Col, Card, Table, TableColumn, Breadcrumb, BreadcrumbItem, Tag
+  DropdownMenu, DropdownItem, Row, Col, Card, Table, TableColumn, Breadcrumb, BreadcrumbItem,
+  Tag, Form, FormItem, Input, Select, Switch, DatePicker, Option, Dialog
 } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/less/index.less'
@@ -14,6 +15,7 @@ import '../api/mock.js'
 
 Vue.config.productionTip = false
 
+Vue.use(Dialog)
 Vue.use(Button)
 Vue.use(Container)
 Vue.use(Header)
@@ -34,6 +36,14 @@ Vue.use(TableColumn)
 Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
 Vue.use(Tag)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Switch)
+Vue.use(DatePicker)
+Vue.use(Option)
+
 Vue.prototype.$http = http
 
 new Vue({
